7e6b0feaef2dcde615b33dac74c6ab8ee0c722be
Sujal Dandhale <136900194+Sujal1123@users.noreply.github.com>
2025-06-28 15:51:52 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'mindease.railway.internal',
+  baseURL: 'http://mindease.railway.internal',
 });
 
 API.interceptors.request.use(config => {
