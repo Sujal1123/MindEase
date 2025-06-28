5efbe3b7396e907bb4e6a4109b311019180d0ba9
Sujal Dandhale <136900194+Sujal1123@users.noreply.github.com>
2025-06-28 15:44:50 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://mind-ease-zeta.vercel.app/",
     methods: ["GET", "POST"],
     credentials: true
   }
