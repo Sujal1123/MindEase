c90290e8118b5a883fe00777b6f2686a1f66e9be
Sujal Dandhale <136900194+Sujal1123@users.noreply.github.com>
2025-06-28 23:42:00 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,7 @@ app.use(cors({
 }));
 
 app.use(express.json());
-app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
+app.use('/uploads', express.static( 'uploads'));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
