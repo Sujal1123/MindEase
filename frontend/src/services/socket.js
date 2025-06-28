1cd53b9b5dac115bb1a674214d71ec580f7ef9bc
Sujal Dandhale <136900194+Sujal1123@users.noreply.github.com>
2025-06-28 16:02:26 +0530

    Update socket.js

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const socket = io('https://mindease.railway.internal');
+const socket = io('https://mindease-production.up.railway.app');
 
 socket.on('connect', () => {
   console.log('Socket connected to server:', socket.id);
